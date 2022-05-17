469f23f2d0be4c87dfe0d8c870122a09ccdc6b31
Zeemari <Zenntia@gmail.com>
2022-05-17 20:18:51 +0100

    Worked with array methods

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React, {useEffect, useState} from 'react'
-
-import {NavBar} from './components'
+// import {NavBar} from './components'
 import {useFetch} from './fetch'
 
 const App = () => {
   const {loading, error, fetchdata}=useFetch();
-  const [keyword, setKeyword]=useState('');
+  const [keyword, setKeyword]=useState('Abuja');
   const [weatherData, setWeatherData]=useState([])
 
   const getWeatherData =async(e)=>{
-    e.preventDefault();
     try {
       const data =await fetchdata(keyword)
       setWeatherData(data);
       console.log(weatherData);
     } catch (error) {}
   }
-  // useEffect(()=>{
-  //   getWeatherData();
-  // })
+
+  const handleFormSubmit = (e)=>{
+    e.preventDefault();
+    getWeatherData();
+  }
+
+  useEffect(()=>{
+    getWeatherData();
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
   if (loading) return <h1>loading...</h1>
   if (error) return <h1>{error}</h1>
   return (
     <>
     Weather Forecast
     <nav> 
-      <form onSubmit={getWeatherData}>
+      <form onSubmit={handleFormSubmit}>
       <input value={keyword} onChange={e =>setKeyword(e.target.value)}/>
       </form>
     </nav>
-
+    <div>
+      {weatherData && (
+        <div>
+          <h1>{weatherData.name}, {weatherData.sys.country}</h1>
+          <h3>Temperature: {weatherData.main.temp}&deg;C</h3>
+          <h3>Humidity: {weatherData.main.humidity}%</h3>
+          <h3>Pressure: {weatherData.main.pressure}Pa</h3>
+          <h4>Description: {weatherData.weather[0].description}</h4>
+          <img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} width='100pz' alt='icon'/>
+        </div>
+      )}
+    </div>
     </>
-
   )
 }
 
